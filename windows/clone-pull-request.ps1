21aa941a4a9c399355c0741b406d6075f48a4cd2
Shubham Agrawal <shubham.agrawal@harness.io>
2021-12-20 16:05:51 +0530

    Erring out early for windows git clone (#10)

--- windows/clone-pull-request.ps1
+++ windows/clone-pull-request.ps1
@@ -1,3 +1,6 @@
+. "${PSScriptRoot}\utility.ps1"
+
+Set-Alias iu Invoke-Utility
 
 Set-Variable -Name "FLAGS" -Value ""
 if ($Env:PLUGIN_DEPTH) {
@@ -5,31 +8,31 @@ if ($Env:PLUGIN_DEPTH) {
 }
 
 if (!(Test-Path .git)) {
-	git init
-	git remote add origin $Env:DRONE_REMOTE_URL
+	iu git init
+	iu git remote add origin $Env:DRONE_REMOTE_URL
 }
 
 if ($Env:PLUGIN_PR_CLONE_STRATEGY -eq "SourceBranch") {
 	Write-Host "+ git fetch ${FLAGS} origin ${Env:DRONE_COMMIT_REF}:"
-	git fetch ${FLAGS} origin "${Env:DRONE_COMMIT_REF}:"
+	iu git fetch ${FLAGS} origin "${Env:DRONE_COMMIT_REF}:"
 	Write-Host "+ git checkout ${Env:DRONE_COMMIT_SHA} -b ${Env:DRONE_SOURCE_BRANCH}"
-	git checkout ${Env:DRONE_COMMIT_SHA} -b ${Env:DRONE_SOURCE_BRANCH}
+	iu git checkout ${Env:DRONE_COMMIT_SHA} -b ${Env:DRONE_SOURCE_BRANCH}
 	exit 0
 }
 
 Write-Host "+ git fetch ${FLAGS} origin +refs/heads/${Env:DRONE_COMMIT_BRANCH}:"
-git fetch ${FLAGS} origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:"
+iu git fetch ${FLAGS} origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:"
 
 if (Test-Path env:DRONE_COMMIT_BEFORE) {
 	# PR clone strategy is merge commit
 	Write-Host "+ git checkout ${Env:DRONE_COMMIT_BEFORE} -b ${Env:DRONE_COMMIT_BRANCH}"
-	git checkout ${Env:DRONE_COMMIT_BEFORE} -b ${Env:DRONE_COMMIT_BRANCH}
+	iu git checkout ${Env:DRONE_COMMIT_BEFORE} -b ${Env:DRONE_COMMIT_BRANCH}
 } else {
 	Write-Host "+ git checkout $Env:DRONE_COMMIT_BRANCH"
-	git checkout $Env:DRONE_COMMIT_BRANCH
+	iu git checkout $Env:DRONE_COMMIT_BRANCH
 }
 
 Write-Host "+ git fetch origin ${Env:DRONE_COMMIT_REF}:"
-git fetch origin "${Env:DRONE_COMMIT_REF}:"
+iu git fetch origin "${Env:DRONE_COMMIT_REF}:"
 Write-Host "+ git merge $Env:DRONE_COMMIT_SHA"
-git merge $Env:DRONE_COMMIT_SHA
+iu git merge $Env:DRONE_COMMIT_SHA
--- windows/clone-tag.ps1
+++ windows/clone-tag.ps1
@@ -1,3 +1,6 @@
+. "${PSScriptRoot}\utility.ps1"
+
+Set-Alias iu Invoke-Utility
 
 Set-Variable -Name "FLAGS" -Value ""
 if ($Env:PLUGIN_DEPTH) {
@@ -6,12 +9,12 @@ if ($Env:PLUGIN_DEPTH) {
 
 if (!(Test-Path .git)) {
 	Write-Host '+ git init';
-	git init
+	iu git init
 	Write-Host "+ git remote add origin $Env:DRONE_REMOTE_URL"
-	git remote add origin "$Env:DRONE_REMOTE_URL"
+	iu git remote add origin "$Env:DRONE_REMOTE_URL"
 }
 
 Write-Host "+ git fetch $FLAGS origin +refs/tags/${Env:DRONE_TAG}:";
-git fetch $FLAGS origin "+refs/tags/${Env:DRONE_TAG}:"
+iu git fetch $FLAGS origin "+refs/tags/${Env:DRONE_TAG}:"
 Write-Host "+ git checkout -qf ${Env:FETCH_HEAD}";
-git checkout -qf FETCH_HEAD
+iu git checkout -qf FETCH_HEAD
