f3d88971add999d37a3eaedd61b77d8b5c1819f7
Shubham Agrawal <shubham.agrawal@harness.io>
2022-12-08 16:10:06 +0530

    Test git win

--- windows/clone-pull-request.ps1
+++ windows/clone-pull-request.ps1
@@ -8,6 +8,8 @@ if ($Env:PLUGIN_DEPTH) {
 }
 
 if (!(Test-Path .git)) {
+	Write-Host "Git init"
+	Write-Host "Remote Url: $Env:DRONE_REMOTE_URL"
 	iu git init
 	iu git remote add origin $Env:DRONE_REMOTE_URL
 }
@@ -20,11 +22,16 @@ if ($Env:PLUGIN_PR_CLONE_STRATEGY -eq "SourceBranch") {
 	exit 0
 }
 
+Write-Host "Remote Url: ${Env:DRONE_REMOTE_URL}"
+Write-Host "Drone commit branch: ${Env:DRONE_COMMIT_BRANCH}"
+Write-Host "Checking set remote"
+git remote -v
 Write-Host "+ git fetch ${FLAGS} origin +refs/heads/${Env:DRONE_COMMIT_BRANCH}:"
 iu git fetch ${FLAGS} origin "+refs/heads/${Env:DRONE_COMMIT_BRANCH}:"
 
 if (Test-Path env:DRONE_COMMIT_BEFORE) {
 	# PR clone strategy is merge commit
+	Write-Host "Drone commit before: ${Env:DRONE_COMMIT_BEFORE}"
 	Write-Host "+ git checkout ${Env:DRONE_COMMIT_BEFORE} -b ${Env:DRONE_COMMIT_BRANCH}"
 	iu git checkout ${Env:DRONE_COMMIT_BEFORE} -b ${Env:DRONE_COMMIT_BRANCH}
 } else {
@@ -32,7 +39,9 @@ if (Test-Path env:DRONE_COMMIT_BEFORE) {
 	iu git checkout $Env:DRONE_COMMIT_BRANCH
 }
 
+Write-Host "Drone commit ref: ${Env:DRONE_COMMIT_REF}"
 Write-Host "+ git fetch origin ${Env:DRONE_COMMIT_REF}:"
 iu git fetch origin "${Env:DRONE_COMMIT_REF}:"
+Write-Host "Drone commit sha: ${Env:DRONE_COMMIT_SHA}"
 Write-Host "+ git merge $Env:DRONE_COMMIT_SHA"
 iu git merge $Env:DRONE_COMMIT_SHA
